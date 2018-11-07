2c1c2ababa0ce42ab713f65b9c2605432b5f5a4f
Galci <gal.lemut@gmail.com>
2018-11-07 14:40:11 +0100

    I added a conflict

--- test.py
+++ test.py
@@ -8,3 +8,5 @@ x=2+2
 print(x==4)
 
 '''This is Klara's first comment on this code, we want to merge comments from all of us here!'''
+"""THERE ARE LLAMAS OUTSIDE OR ALPAKAS I AM NOT SURE SINCE THEY LOOK THE SAME"""
+
