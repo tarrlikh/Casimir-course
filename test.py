da245fb92c9be3656841420a101ec1639466aa6d
Klara <klara-k@gmx.de>
2018-11-07 14:26:31 +0100

    I added a comment in order to merge it with all of you and see whether autmerge does the jib

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ print('hello world')
 print('bye world!')
 
 print('Hi Yarislav! I changed your code')
+'''This is Klara's first comment on this code, we want to merge comments from all of us here!'''
+
